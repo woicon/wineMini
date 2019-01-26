ff8a155e896b6fdeaaa742188bcdd8fb1df8a3dc
b48_100003360888_zhangge <b48_100003360888_zhangge@git.cloud.tencent.com>
2019-01-26 17:31:23 +0800

    up

--- pages/rankingFree/rankingFree.js
+++ pages/rankingFree/rankingFree.js
@@ -1,65 +1,81 @@
 // pages/rankingFree/rankingFree.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
+        list: [
+            {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt:232
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            },
+            {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            },
+            {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            },
+            {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            },
+            {
+                classCover: '../../images/clas.jpg',
+                title: 'SSI国际日本酒讲师教程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+                viewAmt: 232
+            }
 
+        ],
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
     onShareAppMessage: function () {
 
     }
