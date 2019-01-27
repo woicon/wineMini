ed6fb0f307241a2725bd46655cba5db1223c63f3
b48_100003360888_zhangge <b48_100003360888_zhangge@git.cloud.tencent.com>
2019-01-27 16:05:34 +0800

    up

--- pages/memberFav/memberFav.js
+++ pages/memberFav/memberFav.js
@@ -2,7 +2,7 @@
 Page({
 
     data: {
-        currentTab:0,
+        currentTab:2,
         course: [
             {
                 classCover: '../../images/clas.jpg',
@@ -41,7 +41,129 @@ Page({
                 type: 2,
             }
         ],
+        post:[
+            {
+                cover:'../../images/sku.jpg',
+                title:'经典传承碰撞创意灵感',
+                exports:'带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            },
+            {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            },
+            {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }
+        ],
+        list: [{
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        }, {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            salseTotal: 122,
+            viewAmt: 232
+        },
+        {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            salseTotal: 1,
+            viewAmt: 232
+        }, {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        },
+        {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        },
+        {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        }, {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        },
+        {
+            classCover: '../../images/clas.jpg',
+            title: 'SSI国际日本酒讲师教程',
+            subtitle: "专为餐饮人士打造",
+            salePrice: '1999',
+            originalPrice: '2500',
+            type: 2,
+            salseTotal: 334,
+            viewAmt: 232
+        }
+
+        ],
     },
   
-   
+    toggleTab(e){
+        this.setData({
+            currentTab:e.target.dataset.index
+        })
+    }
 })
