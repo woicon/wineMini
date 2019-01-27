60b6249773411d3b34b601c8772ee6a58a6683a1
b48_100003360888_zhangge <b48_100003360888_zhangge@git.cloud.tencent.com>
2019-01-27 15:52:34 +0800

    up

---
+++ pages/memberFav/memberFav.js
@@ -0,0 +1,47 @@
+// pages/memberFav/memberFav.js
+Page({
+
+    data: {
+        currentTab:0,
+        course: [
+            {
+                classCover: '../../images/clas.jpg',
+                title: '苏格兰官方威士忌大使认证课程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 1,
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: '苏格兰官方威士忌大使认证课程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 2,
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: '苏格兰官方威士忌大使认证课程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 2,
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: '苏格兰官方威士忌大使认证课程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 2,
+            }, {
+                classCover: '../../images/clas.jpg',
+                title: '苏格兰官方威士忌大使认证课程',
+                subtitle: "专为餐饮人士打造",
+                salePrice: '1999',
+                originalPrice: '2500',
+                type: 2,
+            }
+        ],
+    },
+  
+   
+})
---
+++ pages/memberVip/memberVip.js
@@ -0,0 +1,63 @@
+// pages/memberVip/memberVip.js
+Page({
+
+    data: {
+        rules: '1、本会员制采用积分制形式；2、购买培训课程及商品可加十倍积分；3、每增加1000分则增加一等级。'
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
