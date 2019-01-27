ec4fda031531fc61d196d25095d535ac05a4b94f
b48_100003360888_zhangge <b48_100003360888_zhangge@git.cloud.tencent.com>
2019-01-27 16:11:32 +0800

    up

---
+++ pages/memberAttention/memberAttention.js
@@ -0,0 +1,34 @@
+
+Page({
+
+    data: {
+        list: [
+            {
+                name: '李美宇',
+                tag: ['三级酒师', '日本讲酒师'],
+                img: '../../images/master.jpg'
+            },
+            {
+                name: '李美宇',
+                tag: ['三级酒师', '日本讲酒师'],
+                img: '../../images/master.jpg'
+            }, {
+                name: '李美宇',
+                tag: ['三级酒师', '日本讲酒师'],
+                img: '../../images/master.jpg'
+            }, {
+                name: '李美宇',
+                tag: ['三级酒师', '日本讲酒师'],
+                img: '../../images/master.jpg'
+            }, {
+                name: '李美宇',
+                tag: ['三级酒师', '日本讲酒师'],
+                img: '../../images/master.jpg'
+            },
+        ]
+    },
+
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pages/memberLike/memberLike.js
@@ -0,0 +1,47 @@
+// pages/masterList/masterList.js
+Page({
+
+    data: {
+        post: [
+            {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            },
+            {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            },
+            {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }, {
+                cover: '../../images/sku.jpg',
+                title: '经典传承碰撞创意灵感',
+                exports: '带给更多消费者极致的葡萄酒体验的愿景，发掘葡萄酒与美食之间味蕾的碰撞'
+            }
+        ],
+    },
+
+    onShareAppMessage: function () {
+
+    }
+})
